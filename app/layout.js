06f3559188306d28fe591db4823b29fedd7a4c3b
AKILI P V S S SRI SWAROOP KUMAR <joyboysskofficially@gmail.com>
2025-11-01 09:43:10 +0530

    updated icons

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,15 @@ export default function RootLayout({ children }) {
       <head>
         {/* <link rel="icon" href="./favicon.ico" /> */}
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/android-chrome-512x512.png" />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/site.webmanifest" />
+
         <meta property="og:image" content="https://www.hairdonations555.org/images/poster.jpg" />
         <meta property="og:image:alt" content="Hair Donations Poster" />
         <meta name="twitter:image" content="https://www.hairdonations555.org/images/poster.jpg" />
